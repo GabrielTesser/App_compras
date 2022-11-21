27aa12406b52b0849084d735531d37ad79783b22
Gabe_Tesser <95052551+GabrielTesser@users.noreply.github.com>
2022-11-21 08:19:48 -0300

    Add files via upload

---
+++ compras/compras/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using compras.Helper;
+using compras.View;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace compras
+{
+    public partial class App : Application
+    {
+
+        static SQLiteDatabaseHelper database;
+
+        public static SQLiteDatabaseHelper Database
+        {
+            get 
+            { 
+                if(database == null)
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"arquivo.db3");
+
+                    database = new SQLiteDatabaseHelper(path);
+                }
+
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new Listagem());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ compras/compras/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using compras.Model;
+using System.Threading.Tasks;
+using SQLite;
+using compras.Model;
+
+namespace compras.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+
+       readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public  Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public void Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+
+        public Task<List<Produto>> getAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public void delete(int id)
+        {
+            _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+    }
+}
